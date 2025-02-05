b356de91d2bfebaf2ef32df05696ded5e19298a3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-05 10:04:05 -0300

    criando primeira model room, classe para a sala em si.

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Room(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=200)
+    messages = models.ManyToManyField("Message")
+    created_at = models.DateTimeField(auto_now_add=True)
